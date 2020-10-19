5fe79e756a9880d7e588b43503ab1178dc6c30e8
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-10-19 23:38:52 +0200

    fix workshop.sh on linux

---
+++ workshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORKSHOP_ID=108170491
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    gmad='../../../bin/gmad'
+    gmpublish='../../../gmpublish'
+else 
+    gmad='../../../bin/linux64/gmad'
+    gmpublish='../../../bin/linux64/gmpublish'
+fi
+
+export LD_LIBRARY_PATH="../../../bin/linux64/"
+
+$gmad create -folder "./" -out "_TEMP.gma"
+$gmpublish update -addon "_TEMP.gma" -id "$WORKSHOP_ID" -icon "icon.jpg"
+
+rm ./_TEMP.gma
