4693ee9d15f3d0cba62571e5dbabe01acbf8d78a
Xerasin <phoenixteam20@gmail.com>
2021-08-29 22:41:02 -0700

    Rename convar

--- lua/autorun/wowozela.lua
+++ lua/autorun/wowozela.lua
@@ -24,7 +24,7 @@ end
 
 if CLIENT then
     wowozela.volume = CreateClientConVar("wowozela_volume", "0.5", true, false)
-    wowozela.clickui = CreateClientConVar("wowozela_clickui", "1", true, false)
+    wowozela.hudtext = CreateClientConVar("wowozela_hudtext", "1", true, false)
     --wowozela.sensitivity = CreateClientConVar("wowozela_sensitivity", "4", true, false)
 
     local function set_sample_index(which, note_index)
